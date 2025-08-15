ceade5b69842006771c896f9697dc6c22600053c
taskinnasif144 <taskinnasif144@gmail.com>
2025-08-16 00:44:37 +0600

    remove broadcasters now sends the details intead of just the id

--- src/core/sockets/handlers/group_room_handler.ts
+++ src/core/sockets/handlers/group_room_handler.ts
@@ -454,7 +454,7 @@ export async function registerGroupRoomHandler(
     }
     io.to(roomId).emit(
       SocketChannels.broadcasterList,
-      Array.from(room.brodcasters)
+      room.broadcastersDetails
     );
   });
 
