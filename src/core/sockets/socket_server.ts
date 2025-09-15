99a407ca345e1cc2767deb39a386c9252deafb5f
taskinnasif144 <taskinnasif144@gmail.com>
2025-09-16 03:04:52 +0600

    refactor(socket): optimize user disconnect handling

--- src/core/sockets/socket_server.ts
+++ src/core/sockets/socket_server.ts
@@ -53,7 +53,7 @@ export default class SocketServer {
   private onlineUsers = new Map<string, string>(); // Map<userId, socketId>
   private disconnectedUsers = new Map<
     string,
-    { timeOut: NodeJS.Timeout, roomId?: string }
+    { timeOut: NodeJS.Timeout; roomId?: string }
   >(); // Map<userId, socketId>
   private hostedRooms = {} as Record<string, RoomData>;
   private userRepo = new UserRepository(User);
@@ -91,10 +91,7 @@ export default class SocketServer {
     this.io.on("connection", (socket) => {
       const userId = socket.handshake.query.userId as string;
       if (userId) {
-        clearTimeout(this.disconnectedUsers.get(userId)?.timeOut);
-        this.disconnectedUsers.delete(userId);
-        this.onlineUsers.set(userId, socket.id);
-        console.log(`User ${userId} connected with socket ID: ${socket.id}`);
+        this.handleUserConnect(userId, socket);
       }
 
       registerGroupRoomHandler(
@@ -113,12 +110,15 @@ export default class SocketServer {
           this.onlineUsers.delete(userId);
           console.log(`User ${userId} disconnected`);
         }
-
-        const timeOut = setTimeout(() => {
-          this.disconnectedUsers.delete(userId);
-          this.hanldeUserDisconnect(userId);
-        }, 5000);
-        this.disconnectedUsers.set(userId, {  timeOut, roomId:  });
+        for (const [roomId, roomData] of Object.entries(this.hostedRooms)) {
+          if (!roomData.members.has(userId)) continue;
+          const timeOut = setTimeout(() => {
+            this.disconnectedUsers.delete(userId);
+            this.hanldeUserDisconnect(userId, roomId, roomData);
+          }, 30000);
+          this.disconnectedUsers.set(userId, { timeOut, roomId });
+          socket.leave(roomId);
+        }
       });
     });
   }
@@ -146,57 +146,63 @@ export default class SocketServer {
     if (this.disconnectedUsers.has(userId)) {
       clearTimeout(this.disconnectedUsers.get(userId)?.timeOut);
       this.disconnectedUsers.delete(userId);
+      if (this.disconnectedUsers.get(userId)?.roomId) {
+        socket.join(this.disconnectedUsers.get(userId)?.roomId!);
+      }
     }
     this.onlineUsers.set(userId, socket.id);
     console.log(`User ${userId} connected with socket ID: ${socket.id}`);
   }
 
-  private hanldeUserDisconnect(userId: string) {
+  private hanldeUserDisconnect(
+    userId: string,
+    roomId: string,
+    roomData: RoomData
+  ) {
     // remove the users and leave the room when diconnected
-    for (const [roomId, roomData] of Object.entries(this.hostedRooms)) {
-      if (roomData.members.has(userId)) {
-        if (roomData.brodcasters.has(userId)) {
-          roomData.brodcasters.delete(userId);
-          roomData.broadcastersDetails = roomData.broadcastersDetails.filter(
-            (broadcaster) => broadcaster._id.toString() !== userId
+
+    if (roomData.members.has(userId)) {
+      if (roomData.brodcasters.has(userId)) {
+        roomData.brodcasters.delete(userId);
+        roomData.broadcastersDetails = roomData.broadcastersDetails.filter(
+          (broadcaster) => broadcaster._id.toString() !== userId
+        );
+        this.io
+          .to(roomId)
+          .emit(
+            SocketChannels.broadcasterList,
+            Array.from(roomData.broadcastersDetails)
           );
-          this.io
-            .to(roomId)
-            .emit(
-              SocketChannels.broadcasterList,
-              Array.from(roomData.broadcastersDetails)
-            );
-        }
-        const objectToDelete = Array.from(roomData.callRequests).find(
-          (request) => request._id.toString() === userId
-        );
-        if (objectToDelete) {
-          roomData.callRequests.delete(objectToDelete);
-          this.io
-            .to(roomId)
-            .emit(
-              SocketChannels.joinCallReqList,
-              Array.from(roomData.callRequests)
-            );
-        }
-        roomData.members.delete(userId);
-        const userDetails = roomData.membersDetails.filter(
-          (member) => member._id.toString() == userId
-        );
-        roomData.membersDetails = roomData.membersDetails.filter(
-          (member) => member._id.toString() !== userId
-        );
-       
-        this.io.to(roomId).emit(SocketChannels.userLeft, userDetails);
       }
-      // Optionally delete empty rooms
-      if (roomData.members.size === 0) {
-        delete this.hostedRooms[roomId];
-        this.io.to(roomId).emit(SocketChannels.roomClosed, {
-          roomId,
-          message: "Room has been closed by the host",
-        });
+      const objectToDelete = Array.from(roomData.callRequests).find(
+        (request) => request._id.toString() === userId
+      );
+      if (objectToDelete) {
+        roomData.callRequests.delete(objectToDelete);
+        this.io
+          .to(roomId)
+          .emit(
+            SocketChannels.joinCallReqList,
+            Array.from(roomData.callRequests)
+          );
       }
+      roomData.members.delete(userId);
+      const userDetails = roomData.membersDetails.filter(
+        (member) => member._id.toString() == userId
+      );
+      roomData.membersDetails = roomData.membersDetails.filter(
+        (member) => member._id.toString() !== userId
+      );
+
+      this.io.to(roomId).emit(SocketChannels.userLeft, userDetails);
+    }
+    // Optionally delete empty rooms
+    if (roomData.members.size === 0) {
+      delete this.hostedRooms[roomId];
+      this.io.to(roomId).emit(SocketChannels.roomClosed, {
+        roomId,
+        message: "Room has been closed by the host",
+      });
     }
   }
 }
