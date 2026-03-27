217e6cb2a3eaad466c6e1e370209a31f62ef7cd8
taskinnasif144 <taskinnasif144@gmail.com>
2026-03-27 16:36:00 +0600

    space removed

--- src/services/family/family_service.ts
+++ src/services/family/family_service.ts
@@ -8,20 +8,20 @@ export interface IFamilyService {
   createFamily(data: IFamily): Promise<IFamilyDocument>;
 }
 
-
 export class FamilyService implements IFamilyService {
-    familyRepository: IFamilyRepository = RepositoryProviders.familyRepositoryProvider;
-    familyMemberRepository: IFamilyMemberRepository = RepositoryProviders.familyMemberRepositoryProvider;
+  familyRepository: IFamilyRepository =
+    RepositoryProviders.familyRepositoryProvider;
+  familyMemberRepository: IFamilyMemberRepository =
+    RepositoryProviders.familyMemberRepositoryProvider;
 
-    async createFamily(data: IFamily): Promise<IFamilyDocument> {
-       //step1: check if user already has a family
-       //step2: check if the user has required level -> 5
-       //step3: check if the user has SVIP-4 or above purchased
-       //step4: if no svip then check for balance 
-       //step5: deduct the balance from the user if no svip
-       //step6: create family
-       //step7: make the leader to be the first member
-       //step8: return the created family
-    }  
-    
+  async createFamily(data: IFamily): Promise<IFamilyDocument> {
+    //step1: check if user already has a family
+    //step2: check if the user has required level -> 5
+    //step3: check if the user has SVIP-4 or above purchased
+    //step4: if no svip then check for balance
+    //step5: deduct the balance from the user if no svip
+    //step6: create family
+    //step7: make the leader to be the first member
+    //step8: return the created family
+  }
 }
